d971271dd6d66303a59ed275fd8b8e9d14e2b10c
Kelvin Gonçalves <kelvingoncalvestecinfo@gmail.com>
2017-03-19 23:48:38 -0300

    1º Parte do Código

--- idade1.1.py
+++ idade1.1.py
@@ -1,3 +1,8 @@
+##Levando em conta cada mês com 30 dias e o ano com 360 dias
+
 dias = int(input("Quantos dias de vida você tem?"))
-idade = dias/365;
-print(idade)
+anos = int(dias/360)
+dias_restantes = dias-(360*anos)
+meses = int(dias_restantes/30)
+dias_restantes2 = dias_restantes-(30*meses)
+print("Voce tem %d anos , %d meses , %d dias de vida "%(anos,meses,dias_restantes2))
