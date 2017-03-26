122f21ddce388bca075774ff78bf0427504941f4
Kelvin Gonçalves <kelvingoncalvestecinfo@gmail.com>
2017-03-26 18:34:41 -0300

    Refactoring

--- Ordenar.py
+++ Ordenar.py
@@ -1,36 +1,24 @@
 n = []
 n1 = []
 want = "y"
-cont = 0
 i = 0
-aux = 0
-tamN = 0
 while want == "y":
     n.append(int(input("Write a number : ")))
     want = input("You want write more numbers ? ")
-    cont = cont + cont
+print("Original Sequence: ")
 print(n)
-aux2 = len(n)
-tamN = len(n)
-while len(n1) != aux2:
-    while i < tamN:
-        cont1 = 0
-        print("N[i]: %d" % (i))
-        for ii in range(0, tamN):
-            print("N[i]: %d  e N[ii]: %d e tam N %s" % (n[i], n[ii], len(n)))
-            print(" ")
+OrigVectorSize = len(n)
+while len(n1) != OrigVectorSize:
+    while i < len(n):
+        cont = 0
+        for ii in range(0, len(n)):
             if n[i] <= n[ii]:
-                cont1 = cont1 + 1
-        print(cont1)
-        print(len(n))
-        if cont1 == len(n):
+                cont = cont + 1
+        if cont == len(n):
             n1.append(n[i])
             n.remove(n[i])
-            print(n1)
-            print(n)
-            print("N1 tam :%d" % (len(n1)))
-            print("N tam :%d" % (len(n)))
-            tamN = len(n)
             i = 0
         else:
             i = i + 1
+print("New Sequence: ")
+print(n1)
