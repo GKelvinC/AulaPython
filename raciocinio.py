dabe0d6b272558d44a403ca2bff73495347a1f5f
Kelvin Gonçalves <kelvingoncalvestecinfo@gmail.com>
2017-06-21 11:10:16 -0300

    Adding problem resolution
    In this commit has a new function that work for explain how this expression works

--- raciocinio.py
+++ raciocinio.py
@@ -3,7 +3,6 @@ if deseja == "V":
     p = True
     q = True
     r = True
-    ii = 9
 else:
     p = input("Digite o valor de P: ( V/F)")
     if p == "V" :
@@ -24,7 +23,7 @@ else:
 # Função contendo toda a logica
 def calculo_expre(p, q, r):
     # (q ^ r)
-    if p & r is True:
+    if q & r is True:
         q1 = True
     else:
         q1 = False
@@ -60,45 +59,92 @@ def calculo_expre(p, q, r):
         Result = True
         # -------------- Fim Lógica -------
     # --- Usado somente para melhorar o design da tabela pois true tem menos caracteres que o false
-    if p is True: p = 'True '
-    if q is True: q = 'True '
-    if r is True: r = 'True '
-    if q1 is True: q1 = 'True '
-    if p1 is True: p1 = 'True '
-    if p2 is True: p2 = 'True '
-    if p3 is True: p3 = 'True '
-    if qF is True: qF = 'True '
-    if Result is True: Result = 'True '
+    if p is True: p = '  V  '
+    if q is True: q = '  V  '
+    if r is True: r = '  V  '
+    if q1 is True: q1 = '  V  '
+    if p1 is True: p1 = '  V  '
+    if p2 is True: p2 = '  V  '
+    if p3 is True: p3 = '  V  '
+    if qF is True: qF = '  V  '
+    if Result is True: Result = '  V  '
+
+    if p is False: p = '  F  '
+    if q is False: q = '  F  '
+    if r is False: r = '  F  '
+    if q1 is False: q1 = '  F  '
+    if p1 is False: p1 = '  F  '
+    if p2 is False: p2 = '  F  '
+    if p3 is False: p3 = '  F  '
+    if qF is False: qF = '  F  '
+    if Result is False: Result = '  F  '
+
 
     return [p, q, r, q1, p1, p2, p3, qF, Result]
-
 #Função para imprimir
 def imprimir_tela(vt):
     print("| %s | %s | %s |  %s    |    %s      |   %s   |   %s   |         %s         |        %s" % (vt[0], vt[1], vt[2], vt[3], vt[4], vt[5], vt[6], vt[7], vt[8]))
+#Função que mostra a resolução do problema
+def resolucao(vt):
+    print(" ")
+    print ( "RESOLUÇÃO:" )
+    print ( "---1ºParte da Resolução: \n   Divida a expressão em partes \n                                             1º Parte : p v ( q ^ r ) / 2º Parte : ( p v q ) ^ ( p v r )  \n    Solucione primeiro os parenteses: " )
+    print ( "    (q ^ r ) = %s /Explicação : Numa conjunção ( ^ ) , o resultado é verdadeiro somente quando os dois lados são verdadeiros "%(vt[3]))
+    input ("    Vamos Prosseguir?(Pressione qualquer tecla!)")
+    print(" ")
+    print ( "---2ºParte da Resolução: \n   Sabendo que ( q ^ r ) é %s resolvemos a 2º parte: "%(vt[3]))
+    print ("     p v (q ^ r ) = %s /Explicação : Numa disjunção ( v ) , o resultado é falso somente quando os dois lados são falsos " % (vt[4]))
+    input ("     Vamos Prosseguir?(Pressione qualquer tecla!)")
+    print(" ")
+    print ( "---3ºParte da Resolução: \n   Resolvemos a segunda parte que e começamos com o ( p v q ): ")
+    print ("     ( p v q ) = %s /Explicação : Numa disjunção ( v ) , o resultado é falso somente quando os dois lados são falsos " % (vt[5]))
+    input ("    Vamos Prosseguir?(Pressione qualquer tecla!)")
+    print(" ")
+    print ( "---4ºParte da Resolução: \n   Após resolver o primeiro parenteses viermos resolver o segundo, ( p v r ): ")
+    print ("     ( p v r ) = %s /Explicação : Numa disjunção ( v ) , o resultado é falso somente quando os dois lados são falsos " % (vt[6]))
+    input ("    Vamos Prosseguir?(Pressione qualquer tecla!)")
+    print(" ")
+    print ( "---5ºParte da Resolução: \n   Após resolver os parenteses agora resolvemos toda a 2º parte, ( p v q ) ^ ( p v r ): ")
+    print ("     ( p v q ) ^ ( p v r ) = %s /Explicação : Numa conjunção ( ^ ) , o resultado é verdadeiro somente quando os dois lados são verdadeiros " % (vt[7]))
+    input ("    Vamos Prosseguir?(Pressione qualquer tecla!)")
+    print(" ")
+    print ( "---Resultado Final: \n   Após resolver ambas as partes Resolvemos a ultima parte, p v ( q ^ r ) <--> ( p v q )^ ( p v r ): ")
+    print ("     p v ( q ^ r ) <--> ( p v q )^ ( p v r ) = %s /Explicação : Numa Bi-Condição ( <--> ) , \n o resultado é verdadeiro somente quando ambas as opções forem iguais ( Ex: F e F OU V e V ) " % (vt[8]))
+    print ( " " )
+#Função que mostra a expressão a ser resolvida
+def expressao():
+    print("Expressão: p v ( q ^ r ) <--> ( p v q )^ ( p v r )")
+    print("---------------------------------------------------------------")
+    print("|   p   |   q   |   r   | ( q ^ r ) | p v ( q ^ r ) | ( p v q ) | ( p v r ) | ( p v q ) ^ ( p v r ) | p v ( q ^ r ) <--> ( p v q )^ ( p v r )")
+#IF que decide se Monta uma tabela intera ou somente uma linha
+if deseja != "V":
+    vt = calculo_expre ( p, q, r )
+    resolucao(vt)
+    expressao ()
+    imprimir_tela ( vt )
+else:
+    # Loop que monta a tabela
+    i = 1
+    expressao()
+    while i < 9:
+        if i == 2:
+            q = False
+            r = False
+        if i == 3 or i == 8:
+            q = True
+            r = False
+        if i == 4 or i == 7:
+            q = False
+            r = True
+        if i == 5:
+            p = False
+            q = False
+            r = False
+        if i == 6:
+            q = True
+            r = True
+        vt = calculo_expre(p, q, r)
+        imprimir_tela(vt)
+        i += 1
+finalizar = input("Deseja finalizar o programa ?")
 
-print("Expressão: p v ( q ^ r ) <--> ( p v q )^ ( p v r )")
-print("---------------------------------------------------------------")
-print("|   p   |   q   |   r   | ( q ^ r ) | p v ( q ^ r ) | ( p v q ) | ( p v r ) | ( p v q ) ^ ( p v r ) | p v ( q ^ r ) <--> ( p v q )^ ( p v r )")
-
-# Loop que monta a tabela
-i = 1
-while i < ii:
-    if i == 2:
-        q = False
-        r = False
-    if i == 3 or i == 8:
-        q = True
-        r = False
-    if i == 4 or i == 7:
-        q = False
-        r = True
-    if i == 5:
-        p = False
-        q = False
-        r = False
-    if i == 6:
-        q = True
-        r = True
-    vt = calculo_expre(p, q, r)
-    imprimir_tela(vt)
-    i += 1
